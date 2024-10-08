43bb3a1c4b0a7ca7795700b0089d942d5578050a
Pranathi-Reddy-Kudithi <164852543+Pranathi-Reddy-Kudithi@users.noreply.github.com>
2024-10-08 23:48:46 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,21 +1,4 @@
-'''import fuzzer
-import spacy
-import newmodel
-import streamlit as st
-nlp = spacy.load("en_core_web_trf")
-text=""
-message =st.text_area("Type your message here:", height=100)
-submit_button = st.button("Submit")
-if submit_button:
-    if message:
-        text=message
-        st.write("Message submitted successfully.")
-    else:
-        st.write("Please enter a message.")
-result = newmodel.extract(nlp,text)
-for word in result:
-    temp , ind,type= fuzzer.fuzzy_search("ASCII.csv", word)
-    st.write(f"The word {word} is found at index {ind} and the canonical name is {temp} and is in the {type} table")'''
+
 import fuzzer   
 import spacy
 import newmodel
